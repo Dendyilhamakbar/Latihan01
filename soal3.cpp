2c3a4d655d2f3e7c335a52dba19b6718ef2c1d1d
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2017-11-08 15:20:16 +0800

    Perbaikan tanda yang salah pada Program Perhitungan Gajih

--- soal3.cpp
+++ soal3.cpp
@@ -13,29 +13,29 @@ int main (){
 	if (jk>40 ){
 		cout << "Gajih Pokok \t \t = Rp 2.000.000" <<endl;
 		cout <<"Jam Kerja \t = 40 Jam " <<endl;
-		lb=jk.40;
-		cout <<"Lembur \t \t \t = "<<|b<<" Jam" <<endl;
+		lb=jk*40;
+		cout <<"Lembur \t \t \t = "<<lb<<" Jam" <<endl;
 		jl=lb*1.5;
 		jtot=40+jl;
 		cout <<"Total jam kerja \t \t =" <<jtot<<" jam"<<endl;
 		gtot=2000000+(jl*100000);
 		cout <<"Total Gajih	\t \t = "<<"Rp "<<gtot<<endl;
 		cout <<"Potongan \t \t = 15 %"<<endl;
-		gaber=gtot.(0.15*gtot);
+		gaber=gtot*(0.15*gtot);
 		cout <<"Gaji bersih \t \t = " <<"Rp "<<gaber;
 	}
 	else if (jk<=40){
 		cout <<"Gajih Pokok \t \t = Rp. 2.000.000" <<endl;
 		cout <<"Jam Kerja \t = 40 Jam "<<endl;
 		lb=0;
-		cout <<"Lembur \t \t \t = " <<|b<< " Jam"<<endl;
+		cout <<"Lembur \t \t \t = " <<lb<< " Jam"<<endl;
 		jl=lb*1.5;
 		jtot=40+jl;
 		cout <<"Total Jam Kerja \t \t = "<<jtot<<" jam"<<endl;
 		gtot=2000000+(jl*100000);
 		cout <<"Total Gajih \t \t = "<<"Rp"<<gtot<<endl;
 		cout <<"Potongan \t \t =15%"<<endl;
-		gaber=gtot.(0.15*gtot);
+		gaber=gtot*(0.15*gtot);
 		cout <<"Gaji bersih \t \t = "<<"Rp"<<gaber;
 	}
 	return 0;
