df3a6839d554ecf40ab32765afb0c790f10543c8
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2017-11-08 14:38:10 +0800

    Perbaikan pada Variable X,Y,Z yang kapital menjadi huruf kecil

--- soal1.cpp
+++ soal1.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main (){
 	int x,y,z;
-	cout << "masukkan nilai X \t \t = ";cin>>X;
+	cout << "masukkan nilai X \t \t = ";cin>>x;
 	cout << "masukkan nilai Y \t \t = ";cin>>y;
-	if (X < Y){
-		Z=Y-X;
+	if (x < y){
+		z=y-x;
 	}
 	else {
-		Z=X-Y;
+		z=x-y;
 	}
-	cout << "Hasil Pengurangan \t = "<<Z;
+	cout << "Hasil Pengurangan \t = "<<z;
 }
