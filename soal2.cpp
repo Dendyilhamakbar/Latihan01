7e28bf08efd5e0430fca05affab12342d6f38064
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2017-11-08 14:55:24 +0800

    Pembuatan inputan Program perhitungan Persegi panjang dan segitiga

---
+++ soal2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main (){
+	int x,y,z;
+	char r;
+	
+	cout <<"Program Hitung Luas"<<endl;
+	cout <<"---------------------"<<endl;
+	cout <<"P = Persegi Panjang" <<endl;
+	cout <<"S = Segitiga"<<endl;
+	cout <<"Ketikkan huruf [P/S] = ";cin>>r;
+	
+	if (r=='P'||r=='p'){
+		cout << "Menghitung Luas Persegi Panjang "<<endl;
+		cout <<"\n";
+		cout <<"Masukkan nilai Panjang  \t = ";cin>>x;
+		cout <<"Masukkan nilai Lebar \t = ";cin>>y;
+		z=x*y;
+		cout<<"Luas Persegi panjang \t = " <<z<<" cm";
+	}
+	else if {}
+	cout << "Menghitung Luas Segitiga"<<endl;
+	cout <<"\n";
+	cout <<"Masukkan nilai atas \t = ";cin>>x;
+	cout <<"Masukkan nilai bawah \t = ";cin>>y;
+	z=0.5*x*y;
+	cout <<"Luas Segitiga \t \t = " <<z<<" cm";
+	}
+	else {
+		cout <<"Inputan anda tidak valid !!!!!! ";
+	}
+	return 0;
+}
+
+
+
