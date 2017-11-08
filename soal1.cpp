1eda3435ced5b19861d1d954b987e9d037797abc
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2017-11-08 14:37:12 +0800

    Pembuatan scrip hasil pengurangan

---
+++ soal1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int x,y,z;
+	cout << "masukkan nilai X \t \t = ";cin>>X;
+	cout << "masukkan nilai Y \t \t = ";cin>>y;
+	if (X < Y){
+		Z=Y-X;
+	}
+	else {
+		Z=X-Y;
+	}
+	cout << "Hasil Pengurangan \t = "<<Z;
+}
